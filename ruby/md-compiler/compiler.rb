f4ba67ef6be162ad747291a53ebbacab885dbe85
John Mayou <john@johnmayou.com>
2025-01-30 23:40:41 -0600

    headers and paragraphs

--- ruby/md-compiler/compiler.rb
+++ ruby/md-compiler/compiler.rb
@@ -1,11 +1,146 @@
+require 'json'
+
 module Compiler
+  class << self
+    def compile(md)
+      tks = Lexer.new(md).tokenize
+      ast = Parser.new(tks).parse
+      CodeGen.new(ast).gen
+    end
+  end
+
   class Lexer
+
+    Token = Struct.new(:type, :attrs)
+
     def initialize(md)
-      @md = md
+      @md = String.new(md)
     end
 
     def tokenize
-      ''
+      tks = []
+
+      @md.lstrip!
+      while !@md.empty?
+        if @md =~ /\A((?:######|#####|####|###|##|#) .+)/
+          text = $1
+          size = 0
+          while text[0] == '#'
+            size += 1
+            text.slice!(0, text[1] == ' ' ? 2 : 1)
+          end
+          tks.push Token.new(:header, {size:, text:})
+          lslice($1.size)
+        elsif @md =~ /\A(\n)/
+          tks.push Token.new(:newl)
+          lslice(1)
+        elsif @md =~ /\A(.+)/
+          tks.push Token.new(:text, {text: $1})
+          lslice($1.size)
+        else
+          raise RuntimeError, "Couldn't match token on '#{@md}'"
+        end
+
+        @md.rstrip!
+      end
+
+      tks
+    end
+
+    def lslice(count)
+      @md.slice!(0, count)
+    end
+  end
+
+  class Parser
+    
+    NodeRoot = Struct.new(:children)
+    NodeHeader = Struct.new(:size, :text)
+    NodeText = Struct.new(:text)
+
+    def initialize(tks)
+      @tks = tks
+    end
+
+    def parse
+      ast = NodeRoot.new(children: [])
+      
+      while !@tks.empty?
+        ast.children << (
+          if peek(:header)
+            parse_header
+          elsif peek(:text)
+            parse_text
+          else
+            raise RuntimeError, "Unable to parse tokens:\n#{JSON.pretty_generate(@tks)}"
+          end
+        )
+      end
+
+      ast
+    end
+
+    private
+
+    def parse_header
+      token = consume(:header)
+      consume(:endl) if peek(:endl)
+      NodeHeader.new(size: token.attrs[:size], text: token.attrs[:text])
+    end
+
+    def parse_text
+      token = consume(:text)
+      consume(:endl) if peek(:endl)
+      NodeText.new(text: token.attrs[:text])
+    end
+
+    def peek(type)
+      (token = @tks[0]) && token.type == type
+    end
+
+    def consume(type)
+      token = @tks.shift
+      if token.nil?
+        raise RuntimeError, "Expected to find token type #{type} but did not find a token"
+      elsif token.type != type
+        raise RuntimeError, "Expected to find token type #{type} but found #{token.type}"
+      end
+      token
+    end
+  end
+
+  class CodeGen
+    def initialize(ast)
+      @ast = ast
+    end
+
+    def gen
+      html = String.new
+
+      @ast.children.each do |node|
+        html << (
+          case node
+          when Parser::NodeHeader
+            gen_header(node)
+          when Parser::NodeText
+            gen_text(node)
+          else
+            raise RuntimeError, "Invalid node: #{node}"
+          end
+        )
+      end
+
+      html
+    end
+
+    private
+
+    def gen_header(node)
+      "<h#{node.size}>#{node.text}</h#{node.size}>"
+    end
+
+    def gen_text(node)
+      "<p>#{node.text}</p>"
     end
   end
 end
--- ruby/md-compiler/compiler_test.rb
+++ ruby/md-compiler/compiler_test.rb
@@ -3,9 +3,91 @@ require 'minitest/autorun'
 require_relative 'compiler.rb'
 
 module Compiler
+  class TestCompile < Minitest::Test
+    [
+      {md: 'text', expected: '<p>text</p>'},
+      {md: 'text #', expected: '<p>text #</p>'},
+    ].each do |tc, i|
+      define_method("test_compile_text_#{i}") do
+        assert_equal tc[:expected], Compiler.compile(tc[:md])
+      end
+    end
+
+    [
+      {md: '# text', expected: '<h1>text</h1>'},
+      {md: '## text', expected: '<h2>text</h2>'},
+      {md: '### text', expected: '<h3>text</h3>'},
+      {md: '#### text', expected: '<h4>text</h4>'},
+      {md: '##### text', expected: '<h5>text</h5>'},
+      {md: '###### text', expected: '<h6>text</h6>'},
+    ].each_with_index do |tc, i|
+      define_method("test_compile_header_#{i}") do
+        assert_equal tc[:expected], Compiler.compile(tc[:md])
+      end
+    end
+  end
+
   class TestLexer < Minitest::Test
-    def test_tokenize
-      assert_equal '', Lexer.new('').tokenize
+    def tokenize(md)
+      Lexer.new(md).tokenize
+    end
+
+    def test_tokenize_blank
+      assert_equal [], tokenize('')
+    end
+
+    def test_tokenize_text
+      assert_equal [Lexer::Token.new(:text, {text: 'text'})], tokenize('text')
+    end
+
+    def test_tokenize_header
+      assert_equal [Lexer::Token.new(:header, {size: 1, text: 'text'})], tokenize('# text')
+    end
+  end
+
+  class TestParser < Minitest::Test
+    def parse(tks)
+      Parser.new(tks).parse
+    end
+
+    def ast_root(*children)
+      Parser::NodeRoot.new(children:)
+    end
+
+    def test_parse_blank
+      assert_equal ast_root, parse([])
+    end
+
+    def test_parse_text
+      assert_equal ast_root(Parser::NodeText.new(text: 'text')),
+        parse([Lexer::Token.new(:text, {text: 'text'})])
+    end
+
+    def test_parse_header
+      assert_equal ast_root(Parser::NodeHeader.new(size: 1, text: 'text')),
+        parse([Lexer::Token.new(:header, {size: 1, text: 'text'})])
+    end
+  end
+
+  class TestCodeGen < Minitest::Test
+    def gen(ast)
+      CodeGen.new(ast).gen
+    end
+
+    def ast_root(*children)
+      Parser::NodeRoot.new(children:)
+    end
+
+    def test_gen_blank
+      assert_equal '', gen(ast_root)
+    end
+
+    def test_gen_text
+      assert_equal '<p>text</p>', gen(ast_root(Parser::NodeText.new(text: 'text')))
+    end
+
+    def test_gen_header
+      assert_equal '<h1>text</h1>', gen(ast_root(Parser::NodeHeader.new(size: 1, text: 'text')))
     end
   end
 end
