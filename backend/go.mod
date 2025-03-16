5ac077981709da3df4b05e736939f24d7f0c8c0e
John Mayou <john@johnmayou.com>
2025-03-16 18:54:09 -0500

    initial backend

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/john-mayou/resume-site/backend
+
+go 1.23.4
---
+++ backend/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "ok"}`))
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/api/health", healthCheckHandler)
+
+	port := 8080
+	fmt.Printf("Server running at http://localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
