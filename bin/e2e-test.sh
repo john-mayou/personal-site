d006ee11784f586e8ca0e52aba0faddb944723a6
John Mayou <john@johnmayou.com>
2025-04-15 22:35:22 -0500

    dont hang on test failure so we can remove the test containers

--- bin/e2e-test.sh
+++ bin/e2e-test.sh
@@ -5,6 +5,13 @@ docker-compose -f "$ROOT_DIR/docker-compose.ci.yml" up --build -d
 
 cd "$ROOT_DIR/tests/e2e"
 npm install
-npx playwright test
+npx playwright test --reporter=html
+success=$?
 
 docker-compose -f "$ROOT_DIR/docker-compose.ci.yml" down
+
+if [ $success -ne 0 ]; then
+  npx playwright show-report
+fi
+
+exit $success
